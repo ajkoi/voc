10249ae2980de39f71386cf62a270fec649ff59a
ajkoi <felix.yiou.alias.yeou@gmail.com>
2025-04-16 17:45:07 +0200

    forgot mainv3.py...

---
+++ mainv3.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import csv_database
+import random
+import time
+import os
+
+class fenetre:
+    def __init__(self):
+        self.main = tk.Tk()
+        self.main.title("voc")
+        
