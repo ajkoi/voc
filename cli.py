97323a8825c27d437d660450572dd5757010b28a
felix <felix.yiou.alias.yeou@gmail.com>
2025-04-19 23:00:12 +0200

    cli working!

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
 import csv_database as csv
 from random import randint
+from time import time
 
 
 class exo:
@@ -11,23 +12,32 @@ class exo:
             pass
 
     def choose_line(self):
-        return (lambda rand: (self.data[rand], rand))(randint(1, len(self.data) - 1))
+        return (lambda rand: (self.data.data[rand], rand))(
+            randint(1, len(self.data.data) - 1)
+        )
 
     def exercice(self):
         print(
             "mettre une virgule et un espace entre chaque mot (et les mettre dans l'ordre)"
         )
-        while len(self.data) > 1:
+        time_start = time()
+        while len(self.data.data) > 1:
             to_guess, line = self.choose_line()
             which = randint(0, len(to_guess) - 1)
             print(f"Quel sont les autres formes de {to_guess[which]} ?")
             answer = input().split(", ")
-            answer = answer[0:which] + to_guess[which] + answer[which:]
+            answer = answer[0:which] + [to_guess[which]] + answer[which:]
             if answer == to_guess:
                 print("C'est bon")
-                del self.data[line]
+                del self.data.data[line]
                 self.found += to_guess
+            else:
+                print(f"ce n'est pas bon, la réponse était {to_guess}")
+        print(
+            f"Tu as fini, tu as tout trouvé en {round(time() - time_start, 1)} secondes."
+        )
 
 
 if __name__ == "__main__":
     a = exo(path="./vocabulary/csv/allemand/verbes-forts.csv")
+    a.exercice()
--- csv_database.py
+++ csv_database.py
@@ -7,6 +7,6 @@ class Data:
         self.path = path
         if not args:
             with open(self.path, "r", encoding="utf-8") as file:
-                self.data = [i.split(",") for i in file.read().split("\n")]
+                self.data = [i.split(",") for i in file.read().split("\n")][:-1]
         else:
             pass
